1d0375384a394d11d85337629de0a97fa520940b
KeiranChan <keiranchan@outlook.com>
2016-03-19 16:06:56 +0800

    ADD--cs文件
    主要游戏逻辑

---
+++ Assets/GameMenu.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenu : MonoBehaviour {
+
+    public static GameMenu _instance;
+    public GUIText nowScore;
+    public GUIText highScore;
+    public GUITexture startTexture;
+
+    void Awake() {
+        _instance = this;
+        this.gameObject.SetActive(false);
+    }
+
+    public void UpdateScore(float nowScore) {
+        float highScore = PlayerPrefs.GetFloat("score",0);
+
+        if (nowScore > highScore) {
+            highScore = nowScore;
+        }
+
+        PlayerPrefs.SetFloat("score", highScore);
+
+        this.nowScore.text = nowScore + "";
+        this.highScore.text = highScore + "";
+
+        if (Input.GetMouseButtonDown(0) && GameManager._intance.GameState == GameManager.GAMESTATE_END) {
+            Rect rect = startTexture.GetScreenRect();
+            Vector3 mousePos =  Input.mousePosition;
+            if (mousePos.x > rect.x &&
+                mousePos.x < (rect.x + rect.width) &&
+                mousePos.y > rect.y &&
+                mousePos.y < (rect.y + rect.height)
+                ) {
+                    Application.LoadLevel(0);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/PipeUpOrDown.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PipeUpOrDown : MonoBehaviour {
+
+    public AudioSource hitMusic;
+    public AudioSource dieMusic;
+
+	void OnCollisionEnter(Collision other){
+		print ("OnCollisionEnter");
+		if(other.gameObject.tag=="Player"){
+            hitMusic.Play();
+            dieMusic.Play();
+			GameManager._intance.GameState=GameManager.GAMESTATE_END;
+
+		}
+	}
+
+}
